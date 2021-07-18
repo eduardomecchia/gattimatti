023232d8779906bbc97c790b62dffa61b2a3f0dd
Eduardo Mecchia <eduardomecchia273@gmail.com>
2021-07-18 17:55:35 +0200

    Create basic layout and extend it in welcome view, which is renamed to home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends("layouts.app")
+
+@section("title", "Gatti Matti")
+
+@section("content")
+    <h1>Welcome to Gatti Matti</h1>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="it">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>@yield("title", "Gatti Matti")</title>
+        <link rel="shortcut icon" href="favicon.png" type="image/x-icon">
+    </head>
+
+    <body>
+        @yield("content")
+    </body>
+</html>
